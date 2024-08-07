84ac237d7281ae1552416a27893f13a8427f9bba
Brahim Jaayfer <brahimjaayfer@Ibs-MacBook-Pro.fritz.box>
2024-08-07 14:08:33 +0200

    Add basic calculator layout

---
+++ src/App.js
@@ -0,0 +1,13 @@
+import "./App.css";
+import Calculator from "./components/Calculator";
+
+function App() {
+  return (
+    <div className="App">
+      <p>Calculator</p>
+      <Calculator />
+    </div>
+  );
+}
+
+export default App;
---
+++ src/components/Calculator.js
@@ -0,0 +1,26 @@
+function Calculator() {
+  return (
+    <div className="calculator-container">
+      <div className="calculator-item calc-display"></div>
+      <div className="calculator-item">clear</div>
+      <div className="calculator-item">7</div>
+      <div className="calculator-item">8</div>
+      <div className="calculator-item">9</div>
+      <div className="calculator-item">/</div>
+      <div className="calculator-item">5</div>
+      <div className="calculator-item">6</div>
+      <div className="calculator-item">7</div>
+      <div className="calculator-item">-</div>
+      <div className="calculator-item">1</div>
+      <div className="calculator-item">2</div>
+      <div className="calculator-item">3</div>
+      <div className="calculator-item">x</div>
+      <div className="calculator-item">,</div>
+      <div className="calculator-item">0</div>
+      <div className="calculator-item">=</div>
+      <div className="calculator-item">+</div>
+    </div>
+  );
+}
+
+export default Calculator;
