d1c8fb5e3b6ea9ced5ebddad87283ea86a68fa04
Brahim Jaayfer <brahimjaayfer@Ibs-MacBook-Pro.fritz.box>
2024-08-07 14:40:55 +0200

    Implement number input functionality

--- src/components/Calculator.js
+++ src/components/Calculator.js
@@ -1,19 +1,46 @@
+import { useState } from "react";
+
 function Calculator() {
+  //display value
+  const [displayValue, setDisplayValue] = useState(0);
+
+  const inputDigit = function (e) {
+    console.log(e.target.innerText);
+  };
+
   return (
     <div className="calculator-container">
       <div className="calculator-item calc-display"></div>
       <div className="calculator-item">clear</div>
-      <div className="calculator-item">7</div>
-      <div className="calculator-item">8</div>
-      <div className="calculator-item">9</div>
+      <div className="calculator-item" onClick={inputDigit}>
+        7
+      </div>
+      <div className="calculator-item" onClick={inputDigit}>
+        8
+      </div>
+      <div className="calculator-item" onClick={inputDigit}>
+        9
+      </div>
       <div className="calculator-item">/</div>
-      <div className="calculator-item">5</div>
-      <div className="calculator-item">6</div>
-      <div className="calculator-item">7</div>
+      <div className="calculator-item" onClick={inputDigit}>
+        4
+      </div>
+      <div className="calculator-item" onClick={inputDigit}>
+        5
+      </div>
+      <div className="calculator-item" onClick={inputDigit}>
+        6
+      </div>
       <div className="calculator-item">-</div>
-      <div className="calculator-item">1</div>
-      <div className="calculator-item">2</div>
-      <div className="calculator-item">3</div>
+      <div className="calculator-item" onClick={inputDigit}>
+        1
+      </div>
+      <div className="calculator-item" onClick={inputDigit}>
+        2
+      </div>
+      <div className="calculator-item" onClick={inputDigit}>
+        3
+      </div>
       <div className="calculator-item">x</div>
       <div className="calculator-item">,</div>
       <div className="calculator-item">0</div>
